e070b8ed8fb62ec5fc2c8f17f5e95141e0c744b1
Simon de Reuver <dereuversimon@gmail.com>
2023-11-20 22:02:36 +0100

    Created directory and inital test file for graph

---
+++ Graph/tests/test_graph.c
@@ -0,0 +1,13 @@
+// Unit tests for the graph module
+
+#include "graph.h"
+#include <assert.h>
+
+
+int main() {
+    
+    // Add calls to test functions as needed
+
+    printf("All tests passed!\n");
+    return 0;
+}
